f1c127949601acb9f7372b86a54609b06a78f099
Elliot Davies <elliot.a.davies@gmail.com>
2015-03-28 13:41:33 -0400

    Bugfixes to popups

--- castlist.js
+++ castlist.js
@@ -29,11 +29,20 @@ function queryMySQL(name) {
         url: url,
         crossDomain: false,
         success: function(data) {
-            data = data[0];
-            $("p.castlist-info-link a").attr("href", data.search_link);
+            if (data == "") {
+                var search_link = "http://www.thetimes.co.uk/tto/public/sitesearch.do?querystring=";
+                search_link += name + "&x=0&y=0&p=tto&pf=all&bl=on#/tto/public/sitesearch.do?querystring=";
+                search_link += name + "&offset=0&hits=0&sortby=date&order=DESC&bl=on&service=searchframe";
+                
+                $("p.castlist-info-link a").attr("href", search_link);
+                queryWikipediaDesc(name);
+            } else {
+                data = data[0];
+                $("p.castlist-info-link a").attr("href", data.search_link);
 
-            if (data.description) $("div.castlist-info-desc").html(data.description);
-            else queryWikipediaDesc(name);
+                if (data.description) $("div.castlist-info-desc").html(data.description);
+                else queryWikipediaDesc(name);
+            }
         }
     });
 }
